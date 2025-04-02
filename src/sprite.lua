5deddc8240fec42ab3f8c54ffe6a21b1068a68d8
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2025-04-02 20:55:49 +0200

    document out sprite.lua

--- src/sprite.lua
+++ src/sprite.lua
@@ -1,5 +1,5 @@
 -- sprite.lua
-
+-- each sprite expect a "spritesheet", it needs to know how many tiles are on the spritesheet, and makes quads according to it.
 local newSprite = function (img, x, y, spOpt)
     local width = img:getWidth()
     local height = img:getHeight()
@@ -13,37 +13,41 @@ local newSprite = function (img, x, y, spOpt)
     local quads = {}
     for fy = 1, sliceY do
         for fx = 1, sliceX do
-            local quad = love.graphics.newQuad((fx-1) * quadWidth,
-                                                (fy-1) * quadHeight,
-                                                quadWidth,
-                                                quadHeight,
-                                                width,
-                                                height)
+            local quad = love.graphics.newQuad(
+                (fx-1) * quadWidth,
+                (fy-1) * quadHeight,
+                quadWidth,
+                quadHeight,
+                width,
+                height
+            )
             table.insert(quads, quad)
         end
     end
 
     return {
-        x = x or 0,
-        y = y or 0,
-        sx = 1,
-        sy = 1,
-        frame = 1,
-        img = img,
-        quads = quads,
-        width = quadWidth,
-        height = quadHeight,
+        x = x or 0, -- x render position of the sprite
+        y = y or 0, -- y render position of the sprite
+        sx = 1, -- info: sx = scaleX
+        sy = 1, -- info: sy = scaleY
+        frame = 1, -- frame is the current rendered tile of the spritesheet, (for a non-animated sprite this is ALWAYS 1)
+        img = img, -- the spritesheet itself
+        quads = quads, -- every frame of the spritesheet is a quad, so a static sprite will ALWAYS be 1 quad 
+        width = quadWidth, -- size of an individual tile, READONLY do not override.
+        height = quadHeight, -- size of an individual tile, READONLY do not override.
 
-        draw = function (sp)
-            local ox, oy
-            ox = sp.width * 0.5
-            oy = sp.height * 0.5
-            love.graphics.draw(sp.img,
-                                sp.quads[sp.frame],
-                                sp.x, sp.y,
-                                sp.r,
-                                sp.sx, sp.sy,
-                                ox, oy)
+        draw = function (self)
+            -- ox & oy are origin points, we want to have x,y at the center of the sprite
+            local ox = self.width * 0.5
+            local oy = self.height * 0.5
+            love.graphics.draw(
+                self.img,
+                self.quads[self.frame],
+                self.x, self.y,
+                nil, -- normally rotation, but N/A in this project
+                self.sx, self.sy, 
+                ox, oy
+            )
         end,
     }
 end
